db891cfda7d9469a54f7c1fe3511b59e7e02d12e
Matheus-Tenorio <165965733+Matheus-Tenorio@users.noreply.github.com>
2025-06-08 19:22:49 -0300

    Update Procedures.sql
     criando novas store procedure

--- Procedures.sql
+++ Procedures.sql
@@ -17,3 +17,39 @@ BEGIN
 	
 	
 END
+
+
+DELIMITER $$
+
+CREATE PROCEDURE RegistrarCliente (
+    IN p_NomeCliente VARCHAR(255),
+    IN p_EmailCliente VARCHAR(255),
+    IN p_CPF CHAR(11)
+)
+BEGIN
+    -- Verifica se já existe um cliente com o CPF informado
+    IF NOT EXISTS (SELECT 1 FROM Cliente WHERE CPF = p_CPF) THEN
+        INSERT INTO Cliente (NomeCliente, EmailCliente, CPF)
+        VALUES (p_NomeCliente, p_EmailCliente, p_CPF);
+    END IF;
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE RegistrarFuncionario (
+    IN p_NomeFuncionario VARCHAR(255),
+    IN p_EmailFuncionario VARCHAR(255),
+    IN p_CPF CHAR(11),
+    IN p_Salario FLOAT(7,2)
+)
+BEGIN
+    -- Verifica se já existe um funcionário com o CPF informado
+    IF NOT EXISTS (SELECT 1 FROM Funcionario WHERE CPF = p_CPF) THEN
+        INSERT INTO Funcionario (NomeFuncionario, EmailFuncionario, CPF, Salario)
+        VALUES (p_NomeFuncionario, p_EmailFuncionario, p_CPF, p_Salario);
+    END IF;
+END $$
+
+DELIMITER ;
