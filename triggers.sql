ab91253293ed45772ec58f9acd25cffd76b8d7fe
Matheus-Tenorio <165965733+Matheus-Tenorio@users.noreply.github.com>
2025-06-11 15:12:32 -0300

    Update triggers.sql
    nova trigger

--- triggers.sql
+++ triggers.sql
@@ -1,11 +1,29 @@
+-- Trigger 1: Se o novo salário do funcionário for menor que o antigo, impede a atualização, garantindo que o novo salario do funcionario seja obrigatoriamente maior que o salario antigo
 DELIMITER $$
--- Se o novo salário do funcionário for menor que o antigo, impede a atualização
+
 CREATE TRIGGER evitar_reducao_salario
 BEFORE UPDATE ON Funcionario
 FOR EACH ROW
 BEGIN
     IF NEW.Salario < OLD.Salario THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Erro: Não é permitido reduzir o salário de um funcionário.';
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+-- Trigger 2: essa trigger é responsável por padronizar o formato do título do livro, tirando espaços em brancos no incio e fim, e tambem garantindo a primeira letra do titulo ser maiuscula 
+DELIMITER $$
+
+CREATE TRIGGER padronizar_titulo_livro
+BEFORE INSERT ON Livros
+FOR EACH ROW
+BEGIN
+    SET NEW.Titulo = TRIM(NEW.Titulo);
+    
+    IF LENGTH(NEW.Titulo) > 0 THEN
+        SET NEW.Titulo = CONCAT(UPPER(SUBSTRING(NEW.Titulo, 1, 1)), LOWER(SUBSTRING(NEW.Titulo, 2)));
     END IF;
 END$$
 
